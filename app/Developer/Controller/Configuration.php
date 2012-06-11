8cccb39968bcbf0625fcaf60cba54acfb5adfd05
SnatchFrigate <snatchfrigate@gmail.com>
2012-06-11 23:15:10 +0100

    [updated] Developer/Controller/Configuration.php to output the config.php file for now. an editor will be in place soon.

--- app/Developer/Controller/Configuration.php
+++ app/Developer/Controller/Configuration.php
@@ -8,6 +8,9 @@ class Configuration extends Actions
 {
   public function configAction()
   {
-    $config = file_get_contents('')
+    $file_to_edit = $this->get('config')->get('file/config.php');
+    
+    header('content-type: text/plain');
+    return file_get_contents($file_to_edit);
   }
 } 
